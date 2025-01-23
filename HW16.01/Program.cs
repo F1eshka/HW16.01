2d297d0dbab752f9749c79de3b37e9f645147d57
F1eshka <144143888+F1eshka@users.noreply.github.com>
2025-01-23 16:28:22 +0300

    Add files via upload

---
+++ HW16.01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+namespace HW16_01
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process = null;
+
+            ProcessStartInfo processStartInfo = new ProcessStartInfo()
+            {
+                FileName = @"C:\Users\Rog\source\repos\HW16.01\HW16.01\bin\Debug\net8.0\HW16.01.exe",
+                UseShellExecute = false,
+            };
+
+            try
+            {
+                process = Process.Start(processStartInfo);
+                process.WaitForExit();
+
+                Console.WriteLine("Exit Code: " + process.ExitCode);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (process != null)
+                {
+                    process.Dispose();
+                }
+            }
+        }
+    }
+}
---
+++ HW2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace HW2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process = null;
+
+            ProcessStartInfo processStartInfo = new ProcessStartInfo()
+            {
+                FileName = @"C:\Users\Rog\source\repos\HW16.01\HW16.01\bin\Debug\net8.0\HW16.01.exe",
+                UseShellExecute = false,
+            };
+
+            try
+            {
+                process = Process.Start(processStartInfo);
+
+                Console.WriteLine("Виберіть дію: ");
+                Console.WriteLine("1. Очікувати завершення дочірнього процесу");
+                Console.WriteLine("2. Примусово завершити дочірній процес");
+                Console.Write("Ваш вибір: ");
+                string choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    process.WaitForExit();
+                    Console.WriteLine("Exit Code: " + process.ExitCode);
+                }
+                else if (choice == "2")
+                {
+                    process.Kill();
+                    Console.WriteLine("Дочірній процес було примусово завершено");
+                }
+                else
+                {
+                    Console.WriteLine("Неправильний вибір. Завершення програми");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (process != null)
+                {
+                    process.Dispose();
+                }
+            }
+        }
+    }
+}
